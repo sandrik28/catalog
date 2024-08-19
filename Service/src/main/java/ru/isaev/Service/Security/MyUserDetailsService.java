3d39de78b5e73bf77dac16fea9e580a4c46f90cc
theSirD <isaev-daniel17@yandex.ru>
2024-08-19 20:36:49 +0300

    fix: minor fixes

--- Service/src/main/java/ru/isaev/Service/Security/MyUserDetailsService.java
+++ Service/src/main/java/ru/isaev/Service/Security/MyUserDetailsService.java
@@ -5,9 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import ru.isaev.DAO.IOwnerDAO;
 import ru.isaev.Domain.Users.User;
-import ru.isaev.Entities.Owners.Owner;
 import ru.isaev.Repo.UserRepo;
 
 import java.util.Optional;
