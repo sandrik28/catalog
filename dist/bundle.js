241976c9a415bcb8e021a3e5086e35ad6aaa7c21
pluto <roso3600@gmail.com>
2024-08-20 12:42:38 +0300

    config

---
+++ dist/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.tsx":
+/*!***********************!*\
+  !*** ./src/index.tsx ***!
+  \***********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/ts-loader/index.js):\nError: TypeScript emitted no output for C:\\Users\\Egor\\Documents\\study\\T1\\catalog\\src\\index.tsx.\n    at makeSourceMapAndFinish (C:\\Users\\Egor\\Documents\\study\\T1\\catalog\\node_modules\\ts-loader\\dist\\index.js:55:18)\n    at successLoader (C:\\Users\\Egor\\Documents\\study\\T1\\catalog\\node_modules\\ts-loader\\dist\\index.js:42:5)\n    at Object.loader (C:\\Users\\Egor\\Documents\\study\\T1\\catalog\\node_modules\\ts-loader\\dist\\index.js:23:5)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.tsx"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
