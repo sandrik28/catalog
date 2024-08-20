9b420668ffd2409be1e85c276fdf1059d7ec664c
theSirD <isaev-daniel17@yandex.ru>
2024-08-20 18:09:31 +0300

    feat: adds new methods to ProductController

--- Controller/src/main/java/ru/isaev/Controller/ProductController.java
+++ Controller/src/main/java/ru/isaev/Controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.isaev.Controller.Mapper.IMyMapper;
 import ru.isaev.Domain.ProductDtos.ProductDto;
 import ru.isaev.Domain.Products.Product;
+import ru.isaev.Domain.Products.Status;
 import ru.isaev.Service.ProductService.IProductService;
 
 import java.util.List;
@@ -22,6 +23,49 @@ public class ProductController {
     public ProductController(IProductService productService, IMyMapper mapper) {
         this.productService = productService;
         this.mapper = mapper;
+    }
+
+    @GetMapping("/status")
+    public ResponseEntity<List<ProductDto>> getByStatus(@RequestParam(name = "status", required = false) Status status) {
+        return new ResponseEntity<>(
+                mapper.mapListOfProductsToListOfDtos(productService.getProductsByStatus(status)),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/followed_by_user/{id}")
+    public ResponseEntity<List<ProductDto>> getProductsFollowedByUser(@PathVariable Long id) {
+        return new ResponseEntity<>(
+                mapper.mapListOfProductsToListOfDtos(productService.getProductsFollowedByUser(id)),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/title")
+    public ResponseEntity<List<ProductDto>> getByTitle(@RequestParam(name = "title", required = false) String title) {
+        return new ResponseEntity<>(
+                mapper.mapListOfProductsToListOfDtos(productService.getProductsByTitle(title)),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/category")
+    public ResponseEntity<List<ProductDto>> getByCategory(@RequestParam(name = "category", required = false) String category) {
+        return new ResponseEntity<>(
+                mapper.mapListOfProductsToListOfDtos(productService.getProductsByCategory(category)),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/title_and_category")
+    public ResponseEntity<List<ProductDto>> getByTitleAndCategory(
+            @RequestParam(name = "title", required = false) String title,
+            @RequestParam(name = "title", required = false) String category
+    ) {
+        return new ResponseEntity<>(
+                mapper.mapListOfProductsToListOfDtos(productService.getProductsByTitleAndCategory(title, category)),
+                HttpStatus.OK
+        );
     }
 
     @GetMapping("/{id}")
@@ -58,6 +102,30 @@ public class ProductController {
     public ResponseEntity<Void> deleteProductById(@PathVariable Long id) {
         productService.removeProductById(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/subscribe_on_product/{id}")
+    public ResponseEntity<ProductDto> subscribeOnProductById(@PathVariable Long id) {
+        ProductDto productDto = mapper.productToProductDto(productService.subscribeOnProductById(id));
+        return ResponseEntity.status(HttpStatus.CREATED).body(productDto);
+    }
+
+    @PostMapping("/unsubscribe_from_product/{id}")
+    public ResponseEntity<ProductDto> unsubscribeFromProductById(@PathVariable Long id) {
+        ProductDto productDto = mapper.productToProductDto(productService.unsubscribeFromProductById(id));
+        return ResponseEntity.status(HttpStatus.CREATED).body(productDto);
+    }
+
+    @PostMapping("/approve_of_publishing_or_editing/{id}")
+    public ResponseEntity<ProductDto> approveOfPublishingOrEditingProductById(@PathVariable Long id) {
+        ProductDto productDto = mapper.productToProductDto(productService.approveOfPublishingOrEditingProductById(id));
+        return ResponseEntity.status(HttpStatus.CREATED).body(productDto);
+    }
+
+    @PostMapping("/archive/{id}")
+    public ResponseEntity<ProductDto> archiveProductById(@PathVariable Long id) {
+        ProductDto productDto = mapper.productToProductDto(productService.archiveProductById(id));
+        return ResponseEntity.status(HttpStatus.CREATED).body(productDto);
     }
 }
 
