c0bd974a1e9b7dc59330f0f4bcca9f18f9f5e195
theSirD <isaev-daniel17@yandex.ru>
2024-08-20 15:02:21 +0300

    feat: adds new method to ProductService

--- Service/src/main/java/ru/isaev/Service/ProductService/IProductService.java
+++ Service/src/main/java/ru/isaev/Service/ProductService/IProductService.java
@@ -2,7 +2,10 @@ package ru.isaev.Service.ProductService;
 
 import ru.isaev.Domain.Products.Product;
 
+import java.util.List;
+
 public interface IProductService {
+    public List<Product> getAllProducts();
     public void addProduct(Product product);
 
     public Product getProductById(Long id);
