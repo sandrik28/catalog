8b0a3b25ee5d8bb07867e4b5177834d59c6e5e79
pluto <roso3600@gmail.com>
2024-08-20 12:43:57 +0300

    session slice

---
+++ src/05_entities/session/sessionSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface SessionState {
+  userId: string | null;
+  role: string | null;
+}
+
+
+const initialState: SessionState = {
+  userId: "0", 
+  role: "user",
+};
+
+
+const sessionSlice = createSlice({
+  name: 'session',
+  initialState,
+  reducers: {
+    setSession: (state, action: PayloadAction<{ userId: string; role: string }>) => {
+      state.userId = action.payload.userId;
+      state.role = action.payload.role;
+    },
+    clearSession: (state) => {
+      state.userId = null;
+      state.role = null;
+    },
+  },
+});
+
+export const { setSession, clearSession } = sessionSlice.actions;
+
+export default sessionSlice.reducer;
