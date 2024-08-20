b01965d20e3bda68dc4575ca08f7d00264cbd99d
theSirD <isaev-daniel17@yandex.ru>
2024-08-20 17:44:42 +0300

    feat: adds new methods to ProductRepo

--- Repo/src/main/java/ru/isaev/Repo/ProductRepo.java
+++ Repo/src/main/java/ru/isaev/Repo/ProductRepo.java
@@ -2,6 +2,16 @@ package ru.isaev.Repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.isaev.Domain.Products.Product;
+import ru.isaev.Domain.Products.Status;
+
+import java.util.List;
 
 public interface ProductRepo extends JpaRepository<Product, Long> {
+    List<Product> findByStatus(Status status);
+
+    List<Product> findByTitle(String title);
+
+    List<Product> findByCategory(String category);
+
+    List<Product> findByTitleAndCategory(String title, String category);
 }
