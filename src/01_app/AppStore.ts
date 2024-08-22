d6eca6d5425d2dbd5abf5cbb8dd502304fd55bdf
pluto <roso3600@gmail.com>
2024-08-23 02:23:33 +0300

    del space

--- src/01_app/AppStore.ts
+++ src/01_app/AppStore.ts
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import sessionReducer from '@/05_entities/session/sessionSlice';
 import { wishlistReducer } from '@/05_entities/wishlist/model/slice';
 
+
 export const store = configureStore({
   reducer: {
     session: sessionReducer,
