db73099e36333e74ebf85f0d7d3b6d4dc8a42373
pluto <roso3600@gmail.com>
2024-08-23 02:22:17 +0300

    add changing wishlist action

--- src/01_app/AppEnter.tsx
+++ src/01_app/AppEnter.tsx
@@ -5,8 +5,9 @@ import '@/06_shared/ui/base.css'
 import store from './AppStore'
 import { MainPage } from '@/02_pages/main';
 import { ProfilePage } from '@/02_pages/profile/ui/Page/Page';
-import { ProductCard, ProductCardType } from '@/05_entities/product';
+import { ProductCard, type ProductCardType, type ProductId } from '@/05_entities/product';
 import { NewProduct } from '@/02_pages/newProduct';
+import { AddToWishlistIcon } from '@/04_features/wishlist/addToWishlist/ui/AddToWishlistIcon';
 
 
 
@@ -14,22 +15,25 @@ export function AppEnter() {
     const newPostButtonHandler = () => {
 
     }
-    const productCardJson:ProductCardType = {
+
+    
+    const productCardJson: ProductCardType = {
         id: 1,
         title: 'ProductNameTest for test in test01',
         description: 'Product Description is our mission 01 Product Description is our mission 01 Product Description is our mission 01',
         category: 'itAndAllWorld'
-    
     }
+
+
     return (
         <Provider store={store}>
             <Router>
                 <LayoutHeader />
                 <Routes>
-                    <Route path='/' element={<MainPage/>}/>
-                    <Route path='/profile/:id' element={<ProfilePage/>}/>
-                    <Route path='/addNewProduct' element={<NewProduct/>}/>
-                    <Route path='/catalog/all' element={<ProductCard product={productCardJson} info={<span className="product_category">sss</span>} />}/>
+                    <Route path='/' element={<MainPage />} />
+                    <Route path='/profile/:id' element={<ProfilePage />} />
+                    <Route path='/addNewProduct' element={<NewProduct />} />
+                    <Route path='/catalog/all' element={<ProductCard actionSlot={<AddToWishlistIcon productId={productCardJson.id as ProductId} />} product={productCardJson} info={<span className="product_category">sss</span>} />} />
                 </Routes>
             </Router>
         </Provider>
--- src/04_features/wishlist/addToWishlist/ui/AddToWishlistIcon.tsx
+++ src/04_features/wishlist/addToWishlist/ui/AddToWishlistIcon.tsx
@@ -1,8 +1,29 @@
+import React, { useCallback } from 'react'
+import type { ProductId } from '@/05_entities/product'
+import { selectIsInWishlist, toggleWishlistProduct } from '@/05_entities/wishlist'
+import css from './AddToWishlistIcon.module.css';
+import { Icon } from '@/06_shared/ui/Icon/Icon'
+import { useAppDispatch, useAppSelector } from '@/06_shared/model/hooks'
 
-export function AddToWishlistIcon() {
+type Props = {
+  productId: ProductId
+}
+
+export function AddToWishlistIcon({ productId }: Props) {
+  const isInWishlist = useAppSelector(state => selectIsInWishlist(state, productId));
+
+  const dispatch = useAppDispatch();
+  
+  const onClick = useCallback(
+    (e: React.MouseEvent<HTMLElement>) => {
+      e.stopPropagation();
+      e.preventDefault();
+      dispatch(toggleWishlistProduct(productId));
+    },
+    [dispatch, productId],
+  );
+
   return (
-    <>
-      
-    </>
-  )
+    <Icon onClick={onClick} className={css.iconStyle} type={isInWishlist ? 'activeHeart' : 'heart'} />
+  );
 }
