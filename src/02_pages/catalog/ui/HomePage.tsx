a2fcdaa9b5d6a564fb243842ff064d57642a7057
Pluto <roso3600@gmail.com>
2024-08-26 04:49:36 +0300

    add homePageForTests

---
+++ src/02_pages/catalog/ui/HomePage.tsx
@@ -0,0 +1,11 @@
+import { ChooseCategoryWidget } from '@/03_widgets/Category/ChooseCategoryWidget';
+
+
+export const HomePage: React.FC = () => {
+  return (
+    <div>
+      <ChooseCategoryWidget />
+      
+    </div>
+  );
+};
