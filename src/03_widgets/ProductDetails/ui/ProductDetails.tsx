c1754fe81c345e1be1f39869aa09111da518f559
vertsyg <rmdlfg601@mail.ru>
2024-08-26 17:54:31 +0300

    add access control in ProductsDetails

--- src/03_widgets/ProductDetails/ui/ProductDetails.tsx
+++ src/03_widgets/ProductDetails/ui/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { IProductDetails } from '@/05_entities/product/model/types'
+import { IProductDetails, Status } from '@/05_entities/product/model/types'
 import css from './ProductDetails.module.css'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
@@ -6,6 +6,9 @@ import { StatusMessage } from './StatusMessage/StatusMessage'
 import { Roles } from '@/05_entities/user/api/types'
 import { AdminButtons } from '@/04_features/adminButtons'
 import { UserButtons } from './UserButtons/UserButtons'
+import { useEffect } from 'react'
+import { useModal } from '@/06_shared/lib/useModal'
+import { Modal } from '@/06_shared/ui/Modal/Modal'
 
 type Props = {
   product: IProductDetails
@@ -15,6 +18,18 @@ export const ProductDetails = ({ product }: Props) => {
   const userId = useSelector((state: RootState) => state.session.userId);
   const userRole = useSelector((state: RootState) => state.session.role);
   const isOwner = product.ownerId === userId;
+  const { modalContent, modalType, openModal } = useModal();
+  const isViewBlocked = !isOwner && (product.status === Status.ON_MODERATION || product.status === Status.MODERATION_DENIED);
+
+  useEffect(() => {
+    if (isViewBlocked) {
+      openModal('Продукт недоступен для просмотра. Попробуйте позже', 'error')
+    }
+  }, [isViewBlocked, openModal]);
+
+  if (isViewBlocked) {
+    return <Modal type={modalType}><h3>{modalContent}</h3></Modal>
+  }
 
   return (
     <>
