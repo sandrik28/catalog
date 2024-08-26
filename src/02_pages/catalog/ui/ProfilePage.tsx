006e7a104577112d1a64685f924c56326d9e0ab8
Pluto <roso3600@gmail.com>
2024-08-26 04:50:35 +0300

    add profile page fore tests

---
+++ src/02_pages/catalog/ui/ProfilePage.tsx
@@ -0,0 +1,12 @@
+import { ChooseCategoryWidget } from '@/03_widgets/Category/ChooseCategoryWidget';
+import { ProductCardList } from '@/03_widgets/ProductCardList/ui/ProductCardList';
+import React from 'react';
+
+
+export const ProfilePage: React.FC = () => {
+
+  return (
+    <div>
+    </div>
+  );
+};
