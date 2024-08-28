f8b472ec6557fc4a92ea41eb1a79cbb12b1de173
pluto <roso3600@gmail.com>
2024-08-28 11:36:36 +0300

    edit: delete not use includes

--- src/01_app/AppEnter.tsx
+++ src/01_app/AppEnter.tsx
@@ -1,26 +1,20 @@
 import { LayoutHeader } from '@/03_widgets/LayoutHeader'
-import { Provider, useDispatch } from 'react-redux'
+import { Provider} from 'react-redux'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import '@/06_shared/ui/base.css'
 import store from './AppStore'
 import { ProfilePage } from '@/02_pages/profile/ui/Page/Page';
-import { type ProductId } from '@/05_entities/product';
 import { ProductFormPage } from '@/02_pages/newProduct';
-import { AddToWishlistIcon } from '@/04_features/wishlist/addToWishlist/ui/AddToWishlistIcon';
 import { ProductPage } from '@/02_pages/product/ui/Page/Page';
-import { Status } from '@/05_entities/product/model/types';
-import { ProductCardList } from '@/03_widgets/ProductCardList/ui/ProductCardList';
 import { MainPage } from '@/02_pages/main';
 import { mockProductDto } from '@/05_entities/product/api/__mocks__/mockProductDto';
 import { HomePage } from '@/02_pages/catalog/ui/HomePage';
-import { ProfilePageTest } from '@/02_pages/catalog/ui/ProfilePage';
 import { LoginPage } from '@/02_pages/login';
 
 
 
 export function AppEnter() {
     
-    const data = mockProductDto()
 
     return (
         <Provider store={store}>
