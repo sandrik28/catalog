30f96074a3c72a5a238a51c51551d50f1aa6d63b
pluto <roso3600@gmail.com>
2024-08-29 09:11:50 +0300

    add catalog to homepage

--- src/02_pages/main/ui/Page/Page.tsx
+++ src/02_pages/main/ui/Page/Page.tsx
@@ -1,3 +1,4 @@
+import { ChooseCategoryWidget } from '@/03_widgets/Category/ChooseCategoryWidget'
 import { AddNewProduct } from '@/04_features/newProduct/addNewProduct'
 import React from 'react'
 
@@ -5,6 +6,7 @@ export function MainPage() {
   return (
     <main>
       <AddNewProduct/>
+      <ChooseCategoryWidget isMainMenu={true} />
     </main>
   )
 }
