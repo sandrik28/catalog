9b7606ffeb7472ba4d28c91d77c6bc06a61a64f8
vertsyg <rmdlfg601@mail.ru>
2024-08-29 10:07:01 +0300

    edit homepage

--- src/02_pages/main/ui/Page/Page.tsx
+++ src/02_pages/main/ui/Page/Page.tsx
@@ -1,11 +1,15 @@
 import { ChooseCategoryWidget } from '@/03_widgets/Category/ChooseCategoryWidget'
 import { AddNewProduct } from '@/04_features/newProduct/addNewProduct'
+import css from './Page.module.css'
 import React from 'react'
 
 export function MainPage() {
   return (
     <main>
-      <AddNewProduct/>
+      <div className={css.container}>
+        <h1>Цифровые продукты</h1>
+        <AddNewProduct/>
+      </div>
       <ChooseCategoryWidget isMainMenu={true} />
     </main>
   )
