fbbaf585f82206cb06c0310e7afd6c7a2007ca99
pluto <roso3600@gmail.com>
2024-08-29 11:28:45 +0300

    add search

--- src/02_pages/main/ui/Page/Page.tsx
+++ src/02_pages/main/ui/Page/Page.tsx
@@ -1,12 +1,33 @@
 import { ChooseCategoryWidget } from '@/03_widgets/Category/ChooseCategoryWidget'
 import { AddNewProduct } from '@/04_features/newProduct/addNewProduct'
-import React from 'react'
+import { InputSearch } from '@/04_features/search'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import css from './Page.module.css'
+
 
 export function MainPage() {
-  return (
-    <main>
-      <AddNewProduct/>
-      <ChooseCategoryWidget isMainMenu={true} />
-    </main>
-  )
+    const { register } = useForm();
+    const [searchResults, setSearchResults] = useState(null);
+
+    const handleApiResponse = (data: any) => {
+        setSearchResults(data);
+    };
+
+    return (
+        <main>
+            <div className={css.container}>
+                <h1>Цифровые продукты</h1>
+                <AddNewProduct />
+            </div>
+            <InputSearch
+                register={register('searchInput')}
+                onApiResponse={handleApiResponse}
+            />
+            <AddNewProduct />
+
+
+            <ChooseCategoryWidget isMainMenu={true} />
+        </main>
+    )
 }
