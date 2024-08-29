90dbe5e886d840f526f154721f609ef55a147d01
pluto <roso3600@gmail.com>
2024-08-29 14:27:42 +0300

    fix infinity loop of requests

--- src/04_features/search/ui/InputSearch.tsx
+++ src/04_features/search/ui/InputSearch.tsx
@@ -1,24 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDebounce } from '@/06_shared/hooks/useDebounce';
 import css from '@/06_shared/ui/Input/Input.module.css';
 import cn from 'classnames';
 import { fetchSearchResults } from '../api/searchApi';
 import { InputSearchProps } from '../model/types';
-
-
 export const InputSearch = ({ maxLength = 50, value, inputStyle = 'default', register, type, onApiResponse }: InputSearchProps) => {
+    
     const [inputValue, setInputValue] = useState(value || '');
     const debouncedValue = useDebounce(inputValue, 500);
-
+    const isRequestSent = useRef(false);
+    const previousDebouncedValue = useRef(debouncedValue); 
     useEffect(() => {
-        if (debouncedValue) {
-            fetchSearchResults(debouncedValue).then(onApiResponse);
+        
+        if (debouncedValue.trim().length > 0 && debouncedValue !== previousDebouncedValue.current && !isRequestSent.current) {
+            isRequestSent.current = true;
+            fetchSearchResults(debouncedValue)
+                .then(onApiResponse)
+                .finally(() => {
+                    isRequestSent.current = false;
+                });
         }
+
+        // Обновляем предыдущее значение
+        previousDebouncedValue.current = debouncedValue;
     }, [debouncedValue, onApiResponse]);
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(event.target.value);
     };
+
 
     return (
         <input
