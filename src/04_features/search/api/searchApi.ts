3bc7734317c7a4e90a39228ec9760d53c0e4dae7
pluto <roso3600@gmail.com>
2024-08-29 18:25:07 +0300

    fix search add mockFilter

--- src/04_features/search/api/searchApi.ts
+++ src/04_features/search/api/searchApi.ts
@@ -4,13 +4,12 @@ export async function fetchSearchResults(query: string) {
   try {
     //   const response = await fetch(`https://api.example.com/search?q=${query}`);
     //   return await response.json();
-    console.log(query)
 
     return productsMockCards.map(product => ({
       ...product,
       status: product.status as Status,
       timeOfLastApproval: new Date(product.timeOfLastApproval),
-    }));
+    })).filter(product => product.title.includes(query));
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error;
--- src/04_features/search/ui/InputSearch.tsx
+++ src/04_features/search/ui/InputSearch.tsx
@@ -4,14 +4,18 @@ import css from '@/06_shared/ui/Input/Input.module.css';
 import cn from 'classnames';
 import { fetchSearchResults } from '../api/searchApi';
 import { InputSearchProps } from '../model/types';
+
+
+
+
 export const InputSearch = ({ maxLength = 50, value, type, onApiResponse }: InputSearchProps) => {
     
     const [inputValue, setInputValue] = useState(value || '');
     const debouncedValue = useDebounce(inputValue, 500);
     const isRequestSent = useRef(false);
     const previousDebouncedValue = useRef(debouncedValue); 
+
     useEffect(() => {
-        
         if (debouncedValue.trim().length > 0 && debouncedValue !== previousDebouncedValue.current && !isRequestSent.current) {
             isRequestSent.current = true;
             fetchSearchResults(debouncedValue)
@@ -20,8 +24,6 @@ export const InputSearch = ({ maxLength = 50, value, type, onApiResponse }: Inpu
                     isRequestSent.current = false;
                 });
         }
-
-        // Обновляем предыдущее значение
         previousDebouncedValue.current = debouncedValue;
     }, [debouncedValue, onApiResponse]);
 
@@ -32,6 +34,7 @@ export const InputSearch = ({ maxLength = 50, value, type, onApiResponse }: Inpu
 
     return (
         <input
+            id={"search"}
             type={type}
             maxLength={maxLength}
             className={cn(css.root, css.input_text)}
