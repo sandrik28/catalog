cdaa2e9e74666545e89e489092b97fefb4d2b814
pluto <roso3600@gmail.com>
2024-08-29 14:01:18 +0300

    del empty files

--- src/04_features/search/api/searchApi.ts
+++ src/04_features/search/api/searchApi.ts
@@ -1,10 +1,16 @@
+import { Status } from '@/05_entities/product/model/types';
 import { productsMockCards } from '@/06_shared/lib/server/';
 export async function fetchSearchResults(query: string) {
   try {
     //   const response = await fetch(`https://api.example.com/search?q=${query}`);
     //   return await response.json();
     console.log(query)
-    return productsMockCards
+
+    return productsMockCards.map(product => ({
+      ...product,
+      status: product.status as Status,
+      timeOfLastApproval: new Date(product.timeOfLastApproval),
+    }));
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error;
