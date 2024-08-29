0f9f975bb802520b3396c6050b93d06d87ee192c
pluto <roso3600@gmail.com>
2024-08-29 18:24:39 +0300

    fix input remove useForm hook

--- src/02_pages/main/ui/Page/Page.tsx
+++ src/02_pages/main/ui/Page/Page.tsx
@@ -2,21 +2,21 @@ import { ChooseCategoryWidget } from '@/03_widgets/Category/ChooseCategoryWidget
 import { AddNewProduct } from '@/04_features/newProduct/addNewProduct'
 import { InputSearch } from '@/04_features/search'
 import { useState } from 'react'
-import { useForm } from 'react-hook-form';
 import css from './Page.module.css'
 import { ProductPreviewCardDto } from '@/05_entities/product/model/types';
 
 
 
 export function MainPage() {
-    const { register } = useForm();
     const [searchResults, setSearchResults] = useState<ProductPreviewCardDto[]>([]);
 
     const handleApiResponse = (data: ProductPreviewCardDto[]) => {
         setSearchResults(data);
     };
 
+    
     return (
+        
         <main>
             <div className={css.container}>
                 <h1>Цифровые продукты</h1>
@@ -26,7 +26,6 @@ export function MainPage() {
             <InputSearch
                 onApiResponse={handleApiResponse}
             />
-
             <ChooseCategoryWidget isMainMenu={true} />
         </main>
     )
