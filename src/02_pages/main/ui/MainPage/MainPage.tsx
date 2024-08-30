e78b7b3f0e4470693bc31b1405992dfc457b15f2
pluto <roso3600@gmail.com>
2024-08-30 16:30:23 +0300

    del not useble imports

--- src/02_pages/main/ui/MainPage/MainPage.tsx
+++ src/02_pages/main/ui/MainPage/MainPage.tsx
@@ -1,11 +1,13 @@
 import { AddNewProduct } from '@/04_features/newProduct/addNewProduct'
 import css from './Page.module.css'
 import { MainCategoryWidget } from '@/03_widgets/Category/MainCategory/MainCategory'
+import { NotificationDto, NotificationMessage } from '@/05_entities/notification/model/types';
+import { NotificationCard } from '@/05_entities/notification/ui/NotificationCard';
 
-
-
+  
 export function MainPage() {
     return (
+        
         <main>
             <div className={css.container}>
                 <h1>Цифровые продукты</h1>
