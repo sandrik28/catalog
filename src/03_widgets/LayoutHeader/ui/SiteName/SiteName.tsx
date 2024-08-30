bef6646a5934ab6e84f8cdeb92a0c53ae4102a31
pluto <roso3600@gmail.com>
2024-08-30 03:25:05 +0300

    fix header name

--- src/03_widgets/LayoutHeader/ui/SiteName/SiteName.tsx
+++ src/03_widgets/LayoutHeader/ui/SiteName/SiteName.tsx
@@ -4,7 +4,7 @@ import css from './SiteName.module.css'
 export default function SiteName() {
   return (
     <div className={css.root}>
-        <span className="text_header">Сервис для публикации и просмотра цифровых продутков</span>
+        <span className="text_header">Сервис для публикации и просмотра цифровых продуктов</span>
     </div>
   )
 }
