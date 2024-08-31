3dfa6fe9360282aeeeeee72bc361d749391bd739
pluto <roso3600@gmail.com>
2024-08-31 03:52:47 +0300

    newStore

--- src/01_app/AppStore.ts
+++ src/01_app/AppStore.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import sessionReducer from '@/05_entities/session/sessionSlice';
 import { wishlistReducer } from '@/05_entities/wishlist/model/slice';
-
+import { api } from '@/06_shared/api/api'; 
 
 export const store = configureStore({
   reducer: {
     session: sessionReducer,
     wishlist: wishlistReducer,
+    [api.reducerPath]: api.reducer, 
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware), 
 });
 
 export type RootState = ReturnType<typeof store.getState>;
